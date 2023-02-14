01ac18a2ce9601a16b363a0a6ca3ce35410a21eb
Andrey Korolev <andrey.korolev@kingfisher.com>
2023-02-14 15:40:58 +0100

    feat: api endpoints fixed + .env files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: import.meta.env.VITE_DATA_SOURCE_URL,
+  order: import.meta.env.VITE_DATA_SOURCE_URL,
+  import: import.meta.env.VITE_DATA_SOURCE_URL,
+  bff: import.meta.env.VITE_DATA_SOURCE_URL,
+  cart: import.meta.env.VITE_DATA_SOURCE_URL,
 };
 
 export default API_PATHS;
